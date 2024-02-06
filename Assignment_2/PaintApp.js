24ece0d1a692c9b37e05d68f97ad2ddeacd21169
RainbowMod28 <124457667+RainbowMod28@users.noreply.github.com>
2024-02-06 14:41:23 -0600

    Completed Paint App
    Got it fully workin

--- Assignment_2/PaintApp.js
+++ Assignment_2/PaintApp.js
@@ -1,11 +1,16 @@
+
+let brushcolor;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(223);
+  brushcolor = color(0,0,0);
 }
 
-function draw() {
-  background(223);
+function drawColors() {
+ strokeWeight(0);
 //All the color squares
-  stroke(255);
+  
   fill(255,0,0);
   square(0,0,90);
 
@@ -35,10 +40,60 @@ function draw() {
 
   fill(0);
   square(0,810,90);
+  
+}
 
-  //mouse function
-  circle(mouseX, mouseY, 50);
-  if(mouseIsPressed){
-    line(pmouseX, pmouseY,mouseX,mouseY);
+function mousePressed(){
+  if(mouseX < 90)
+  {
+    if(mouseY < 90){
+      brushcolor = color(255,0,0);
+    }
+    else if(mouseY < 180){
+      brushcolor = color(255, 128,0);;
+    }
+    else if(mouseY < 270){
+      brushcolor = color(255, 255,0);
+    }
+    else if(mouseY < 360){
+      brushcolor = color(0,128, 0);
+    }
+    else if(mouseY < 450){
+      brushcolor = color(0,255,255);
+    }
+    else if(mouseY < 540){
+      brushcolor = color(0,0, 255);
+    }
+    else if(mouseY < 630){
+      brushcolor = color(255, 0, 255);
+    }
+    else if(mouseY < 720){
+      brushcolor = color(102, 51, 0);
+    }
+    else if(mouseY < 810){
+      brushcolor = color(255);
+    }
+    else if (mouseY < 900){
+      brushcolor = color(0);
+    }
   }
+}
+
+
+
+function draw(){
+brushX = mouseX;
+brushy = mouseY;
+
+drawColors(); 
+
+//mouse function
+cursor(CROSS);
+
+if(mouseIsPressed){
+  stroke(brushcolor);
+  strokeWeight(50);
+  line(pmouseX, pmouseY,mouseX,mouseY);
+}
+  
 }
